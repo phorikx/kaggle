fe6efd420235e1074042c59a88e33e919a0cf586
Paul Horikx <phorikx@sogyo.nl>
2025-04-15 22:11:27 +0200

    Some stylistic changes testing Air, plus adapted to Windows

--- titanic/R/regression.R
+++ titanic/R/regression.R
@@ -8,24 +8,28 @@ basis_map <- "/home/paul/projects/kaggle/titanic"
 data_map <- file.path(basis_map, "data")
 output_map <- file.path(basis_map, "output")
 functie_map <- file.path(basis_map, "R", "functies")
-list.files(path = functie_map, pattern = "\\.[Rr]$", full.names = TRUE) |> walk(source)
+list.files(path = functie_map, pattern = "\\.[Rr]$", full.names = TRUE) |>
+  walk(source)
 
-titanic_train <- read_csv(file.path(data_map, "train.csv")) 
+titanic_train <- read_csv(file.path(data_map, "train.csv"))
 titanic_test <- read_csv(file.path(data_map, "test.csv"))
 
 titanic_train <- modify_titanic(titanic_train)
-titanic_test <- modify_titanic(titanic_test, is_train = FALSE )
+titanic_test <- modify_titanic(titanic_test, is_train = FALSE)
 
 gen_model <- function(data_train) {
-  glm(Survived ~ Pclass + Sex + IsAlone + FamilySize + Title,
-    data = data_train, family = "binomial")
+  glm(
+    Survived ~ Pclass + Sex + IsAlone + FamilySize + Title,
+    data = data_train,
+    family = "binomial"
+  )
 }
 
 pred <- function(model, data_test) {
   predict(model, data_test, type = "response")
 }
 
-eval <- function(predicted, data_test){
+eval <- function(predicted, data_test) {
   data_test <- data_test |>
     mutate(predicted = predicted) |>
     mutate(eval = abs(as.integer(Survived) - 1 - round(predicted)))
@@ -38,8 +42,16 @@ evaluate_model(titanic_train, gen_model, pred, eval)
 model <- gen_model(titanic_train)
 prediction <- pred(model, titanic_test)
 
-output <- tibble(PassengerId = titanic_test$PassengerId, Survived = prediction) |> 
+output <- tibble(
+  PassengerId = titanic_test$PassengerId,
+  Survived = prediction
+) |>
   mutate(Survived = round(Survived))
 head(output)
 
-write.csv(output, file.path(output_map, "submission_regression.csv"), row.names = FALSE, quote = FALSE)
+write.csv(
+  output,
+  file.path(output_map, "submission_regression.csv"),
+  row.names = FALSE,
+  quote = FALSE
+)
--- titanic/R/titanic.R
+++ titanic/R/titanic.R
@@ -10,53 +10,116 @@ library(readr)
 library(mice)
 library(purrr)
 
-basis_map <- "/home/paul/projects/kaggle/titanic"
+# basis_map <- "/home/paul/projects/kaggle/titanic"
+basis_map <- "C:\\Users\\phori\\Documents\\projects\\kaggle\\titanic"
 data_map <- file.path(basis_map, "data")
 output_map <- file.path(basis_map, "output")
 functie_map <- file.path(basis_map, "R", "functions")
-list.files(path = functie_map, pattern = "\\.[Rr]$", full.names = TRUE) |> walk(source)
+list.files(path = functie_map, pattern = "\\.[Rr]$", full.names = TRUE) |>
+  walk(source)
 
-titanic_train_file <- read_csv(file.path(data_map, "train.csv")) 
+titanic_train_file <- read_csv(file.path(data_map, "train.csv"))
 titanic_test_file <- read_csv(file.path(data_map, "test.csv"))
 
 trim_data <- function(data, is_train = TRUE) {
-  features <- c("Pclass", "Sex", "AgeBin", "FareBin",
-   "Title", "FamilySize", "IsAlone", "FareBin", "Deck", "Embarked", 
-    "LogFare", "Mother", "YoungMiss", "FarePerPerson", "NameLetter",
-  "HasSibling", "HasParent", "LargeFamily", "SmallFamily")
+  features <- c(
+    "Pclass",
+    "Sex",
+    "AgeBin",
+    "FareBin",
+    "Title",
+    "FamilySize",
+    "IsAlone",
+    "FareBin",
+    "Deck",
+    "Embarked",
+    "LogFare",
+    "Mother",
+    "YoungMiss",
+    "FarePerPerson",
+    "NameLetter",
+    "HasSibling",
+    "HasParent",
+    "LargeFamily",
+    "SmallFamily"
+  )
   features <- if (is_train) {
-    c("Survived",features)
-    } else {
-      features
-    }
+    c("Survived", features)
+  } else {
+    features
+  }
 
   data |> select(features)
 }
 
-# Data aanpassen aan 
-titanic_train <- modify_titanic(titanic_train_file) |> 
+# Data aanpassen aan
+titanic_train <- modify_titanic(titanic_train_file) |>
   trim_data()
-titanic_test <- modify_titanic(titanic_test_file, is_train = FALSE) |> trim_data(is_train = FALSE)
+titanic_test <- modify_titanic(titanic_test_file, is_train = FALSE) |>
+  trim_data(is_train = FALSE)
 
 random_forest_model <- train_random_forest(titanic_train)
 gradient_boosting_model <- train_gradient_boosting(titanic_train)
 glm_model <- train_glm(titanic_train)
-select_cols <- c("Survived", "Pclass", "Sex", "AgeBin", "FamilySize",
-"FarePerPerson", "Mother", "IsAlone", "Embarked", "Title")
+select_cols <- c(
+  "Survived",
+  "Pclass",
+  "Sex",
+  "AgeBin",
+  "FamilySize",
+  "FarePerPerson",
+  "Mother",
+  "IsAlone",
+  "Embarked",
+  "Title"
+)
 svm_model <- train_svm(titanic_train, select_cols)
 
-# Make predictions 
-rf_preds <- predict(random_forest_model, newdata = titanic_test, type = "prob")$DidSurvive 
-gbm_preds <- predict(gradient_boosting_model, newdata = titanic_test, type = "prob")$DidSurvive
-x_test <- model.matrix( ~ Pclass + Sex + FarePerPerson + Mother + LogFare + IsAlone + FamilySize + Title + AgeBin + AgeBin*Sex + Pclass * Sex - 1, data = titanic_test)
+# Make predictions
+rf_preds <- predict(
+  random_forest_model,
+  newdata = titanic_test,
+  type = "prob"
+)$DidSurvive
+gbm_preds <- predict(
+  gradient_boosting_model,
+  newdata = titanic_test,
+  type = "prob"
+)$DidSurvive
+x_test <- model.matrix(
+  ~ Pclass +
+    Sex +
+    FarePerPerson +
+    Mother +
+    LogFare +
+    IsAlone +
+    FamilySize +
+    Title +
+    AgeBin +
+    AgeBin * Sex +
+    Pclass * Sex -
+    1,
+  data = titanic_test
+)
 glm_preds <- predict(glm_model, newdata = x_test, type = "prob")$DidSurvive
 
 optimal_weights <- find_optimal_weights(train_data = titanic_train)
 
-ensemble_preds <- optimal_weights[1] * rf_preds + optimal_weights[2] * gbm_preds + optimal_weights[3]* glm_preds
+ensemble_preds <- optimal_weights[1] *
+  rf_preds +
+  optimal_weights[2] * gbm_preds +
+  optimal_weights[3] * glm_preds
 
 # Beste model zoals eerder gemaakt wegschrijven
-output <- tibble(PassengerId = titanic_test_file$PassengerId, Survived = round(ensemble_preds))
+output <- tibble(
+  PassengerId = titanic_test_file$PassengerId,
+  Survived = round(ensemble_preds)
+)
 head(output)
 
-write.csv(output, file.path(output_map, "submission_meta_model.csv"), row.names = FALSE, quote = FALSE)
+write.csv(
+  output,
+  file.path(output_map, "submission_meta_model.csv"),
+  row.names = FALSE,
+  quote = FALSE
+)
